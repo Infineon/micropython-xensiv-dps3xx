9605b3dc8b5368d0668e4560f05f49bd4ec3b89b
Eder Julian <Julian.Eder@infineon.com>
2026-02-03 19:06:36 +0100

    examples: Update to fit new API.

--- examples/measure_pressure_and_temp.py
+++ examples/measure_pressure_and_temp.py
@@ -1,9 +1,9 @@
 from machine import I2C
-import DPS
+import Dps3xx
 from time import sleep
 
 i2c = I2C(scl='P0_2', sda='P0_3', freq=400000)
-dps = DPS.DPS(i2c)
+dps = Dps3xx.Dps3xx(i2c)
 
 for i in range(8):
     t = dps.measureTemperatureOnce()
